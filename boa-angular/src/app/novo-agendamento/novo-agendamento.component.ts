049875340134db68e006dd3d788d019d8c3afee3
cesarquadros <ninoreggae@gmail.com>
2018-04-08 21:47:20 -0300

    incio da criação da view de novo agendamento

---
+++ boa-angular/src/app/novo-agendamento/novo-agendamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'boa-novo-agendamento',
+  templateUrl: './novo-agendamento.component.html',
+  styleUrls: ['./novo-agendamento.component.css']
+})
+export class NovoAgendamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
